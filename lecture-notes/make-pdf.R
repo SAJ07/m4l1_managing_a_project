9977a4678129c9dafb53a49a631b74078020e0d0
SAJ07 <148369570+SAJ07@users.noreply.github.com>
2023-10-30 13:22:05 +0530

    Delete lecture-notes directory

--- lecture-notes/make-pdf.R
+++
@@ -1,9 +0,0 @@
-# Makes pdf slides for all lectures
-lapply(list.files("lecture-notes", pattern = "*.html", recursive = T, full.names = TRUE)[10], 
-       function(file) {
-         xaringan::decktape(
-           file, 
-           output = paste0(tools::file_path_sans_ext(file), ".pdf")
-         )
-       }
-)
